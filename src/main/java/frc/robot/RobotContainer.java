83b806cc765a641519a0163fbbf268ec942e7cfa
okotinek1313 <jhuotattia@gmail.com>
2026-02-07 11:19:20 -0500

    shooting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Manipulator;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -20,10 +22,11 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
+  private final CommandXboxController commandController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
   
   private final Drive drive = new Drive();
+  private final Manipulator manipulator = new Manipulator();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -12,5 +12,9 @@ public class RobotMap {
     public static SparkMax BottomLeft = new SparkMax(0, MotorType.kBrushless); 
     public static SparkMax BottomRight = new SparkMax(0, MotorType.kBrushless); 
 
+    public static SparkMax Intake = new SparkMax(0, MotorType.kBrushless);
+    public static SparkMax Shooter = new SparkMax(0, MotorType.kBrushless);
+
+
     public static XboxController Controller = new XboxController(0);
 }
